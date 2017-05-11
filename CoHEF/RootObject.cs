042e9040080010d5371595fed1895ca64ee6eecf
Walentin Lamonos <walentinlamonos@yahoo.de>
2017-05-11 11:10:46 +0200

    Walki: Commited up to date files.

---
+++ CoHEF/RootObject.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoHEF
+{
+    public class RootObject
+    {
+        public List<string> feld_steiner { get; set; }
+        public List<string> churchill { get; set; }
+        public List<string> woroshilov { get; set; }
+        public List<string> hitler_cats { get; set; }
+        public List<string> downfall { get; set; }
+        public List<string> red_army { get; set; }
+        public List<string> compstomp { get; set; }
+        public List<string> mohairborne { get; set; }
+        public List<string> jungleking { get; set; }
+        public List<string> volkssturm { get; set; }
+        public List<string> danko { get; set; }
+        public List<string> pershing2 { get; set; }
+        public List<string> collateral { get; set; }
+        public List<string> vet4 { get; set; }
+        public List<string> kettenkrad { get; set; }
+        public List<string> isu { get; set; }
+        public List<string> r_btn { get; set; }
+        public List<string> heroes { get; set; }
+        public List<string> stransky { get; set; }
+        public List<string> lendlease { get; set; }
+        public List<string> bledforthis { get; set; }
+        public List<string> crazywilly { get; set; }
+        public List<string> is2construction { get; set; }
+        public List<string> jeep { get; set; }
+        public List<string> wolverine { get; set; }
+        public List<string> bergetiger { get; set; }
+        public List<string> beutepanzer { get; set; }
+        public List<string> rushberlin { get; set; }
+        public List<string> mg42 { get; set; }
+        public List<string> hummel { get; set; }
+        public List<string> kv1 { get; set; }
+        public List<string> grup_steiner { get; set; }
+        public List<string> pershing { get; set; }
+        public List<string> stormtiger { get; set; }
+        public List<string> soviet_prod { get; set; }
+        public List<string> saizew { get; set; }
+        public List<string> furytiger { get; set; }
+        public List<string> wittmann { get; set; }
+        public List<string> bigcats { get; set; }
+        public List<string> oneman { get; set; }
+        public List<string> furysherman { get; set; }
+    }
+}
